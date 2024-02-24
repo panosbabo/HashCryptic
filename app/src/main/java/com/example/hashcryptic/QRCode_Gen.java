8a498437efef968d7816db52c2db4dbbaebd748a
panosbabo <c19764485@mytudublin.ie>
2024-02-24 17:16:42 +0000

    Added QRCode "share to" option

--- app/src/main/java/com/example/hashcryptic/QRCode_Gen.java
+++ app/src/main/java/com/example/hashcryptic/QRCode_Gen.java
@@ -6,24 +6,36 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class QRCode_Gen extends AppCompatActivity {
-
-    private TextView hashtv;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -31,7 +43,8 @@ public class QRCode_Gen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode_gen);
 
-        ImageView imageView = findViewById(R.id.qrcode_image);
+        TextView hashtv;
+        ImageView qrCode = findViewById(R.id.qrcode_image);
         hashtv = findViewById(R.id.hash_txt_view);
 
         int width = 800;
@@ -46,7 +59,7 @@ public class QRCode_Gen extends AppCompatActivity {
 
         try {
             Bitmap bitmap = generateQRCode(hash, width, height);
-            imageView.setImageBitmap(bitmap);
+            qrCode.setImageBitmap(bitmap);
         } catch (WriterException e) {
             e.printStackTrace();
         }
@@ -57,30 +70,71 @@ public class QRCode_Gen extends AppCompatActivity {
 
             @Override
             public void onClick(View view){
-
-                // Passing hash value to new share to intent
-//                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-//                shareIntent.setType("text/plain");
-//                shareIntent.putExtra();
-//                startActivity(Intent.createChooser(shareIntent, "Share to:"));
-
+                // Passing ImageView to be handled for share to option
+                shareImage(qrCode);
             }
         });
 
     }
 
+    private void shareImage(ImageView qr_code) {
+
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) qr_code.getDrawable();
+        if (bitmapDrawable == null) {
+            Toast.makeText(this, "Image not found", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Creating Executor Service to handle image processing in a different thread than main
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        service.execute(new Runnable() {
+            @Override
+            public void run() {
+                // Creating temporary file for the image path
+                File imagePath = new File(getExternalCacheDir(), "images");
+                File newFile = new File(imagePath, "image.png");
+                Uri contentUri = FileProvider.getUriForFile(getApplicationContext(), "com.example.hashcryptic.fileprovider", newFile);
+
+                // Generating bitmap for the image
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                File cachePath = new File(getExternalCacheDir(), "images");
+                cachePath.mkdir(); // Create the cache directory if not exists
+                try {
+                    // Overwrite existing file
+                    FileOutputStream stream = new FileOutputStream(cachePath + "/image.png");
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // Creating new intent for share option as an image
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("image/*");
+                shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+                startActivity(Intent.createChooser(shareIntent, "Share image to"));
+            }
+        });
+    }
+
     private Bitmap generateQRCode(String text, int width, int height) throws WriterException {
+        // HashMap creation for the QRCode image to be generated
         Map<EncodeHintType, Object> hints = new HashMap<>();
         hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
 
+        // Using QRCodeWriter to create a barcode format for the given string hash value
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints);
 
+        // Bitmap creation of the given barcode format hash value
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                bitmap.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+        // Indented for loop to cycle through the entire size of the QRCode image
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                // Black & white pixel generation by the given Hashmap depended of the given string hash value
+                bitmap.setPixel(i, j, bitMatrix.get(i, j) ? Color.BLACK : Color.WHITE);
             }
         }
 
